680ad6a74f0e20c3345a2dab552c9c0708cfa43d
121onto <121onto@gmail.com>
2019-02-16 13:48:22 -0800

    Updates revise notation used for acov estimation

--- py_metrics/base.py
+++ py_metrics/base.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 """Base classes."""
 from __future__ import print_function
 from __future__ import absolute_import
@@ -24,6 +26,7 @@ class Reg(object):
         self.x_cols = x_cols
         self.y_col = y_col
         self.k = len(x_cols)
+        self.n = None
 
         # Data arraays
         self.x = None
@@ -58,7 +61,8 @@ class Reg(object):
         # Prepare the data
         self.x = x = frame[self.x_cols].copy(deep=True).astype(np.float32).values
         self.y = y = frame[self.y_col].copy(deep=True).astype(np.float32).values
-        n, k = frame.shape[0], self.k
+        self.n = frame.shape[0]
+        n, k = self.n, self.k
 
         # other operations
         self.qxx = np.matmul(np.transpose(x), x)
@@ -98,27 +102,32 @@ class Reg(object):
         return np.matmul(x, self.beta)
 
 
-    def acov(self, estimator='norm_v0'):
+    def acov(self, estimator='v_hc3'):
         if not self._is_fit:
             raise RuntimeError('''
             You must run `Reg.fit` before running `Reg.predict`.''')
 
-        acov = self.qxx_inv * (self.o_hat ** 2)
-        if estimator == 'norm_v0':
-            return acov
-        elif estimator == 'norm_v1':
+        if estimator == 'v_0':
+            return self.qxx_inv * (self.s_hat ** 2)
+        elif estimator == 'v_hc0':
             e_hat, x = self.e_hat, self.x
-        elif estimator == 'norm_v2':
-            e_hat, x = self.e_til, self.x
-        elif estimator == 'norm_v3':
+            norm = 1
+        elif estimator == 'v_hc1':
+            e_hat, x = self.e_hat, self.x
+            norm = self.n / (self.n - self.k)
+        elif estimator == 'v_hc2':
             e_hat, x = self.e_bar, self.x
+            norm = 1
+        elif estimator == 'v_hc3':
+            e_hat, x = self.e_til, self.x
+            norm = 1
 
         one = three = self.qxx_inv
         two = np.matmul(
             np.multiply(np.transpose(x), e_hat),
             np.multiply(e_hat[:, np.newaxis], x)
         )
-        acov = np.matmul(np.matmul(one, two), three)
+        acov = norm * np.matmul(np.matmul(one, two), three)
         return acov
 
 
@@ -127,8 +136,34 @@ class Reg(object):
         return np.diag(acov)
 
 
+    def msfe(self):
+        """Computes the mean-square forecast error (for a sample of size n-1).
+        The forecast error is the difference between the actual value for y and
+        it's point forecast. This is the forecast.  The mean-squared forecast
+        error (MSFE) is its expected squared value.
+
+        SOURCES:
+
+            - Hansen, Chapter 4.121onto
+
+        """
+        if not self._is_fit:
+            raise RuntimeError('''
+            You must run `Reg.fit` before running `Reg.predict`.''')
+        return self.o_til
+
+
     def leverage(self):
-        # SOURCE: https://stackoverflow.com/a/39534036/759442
+        """The leverage values hii measure how unusual the ith observation xi is
+        relative to the other values in the sample. A large hii occurs when xi
+        is quite different from the other sample values.
+
+        SOURCES:
+
+            - https://stackoverflow.com/a/39534036/759442
+            - Hansen, Chapter 3.19
+
+        """
         if not self._is_fit:
             raise RuntimeError('''
             You must run `Reg.fit` before running `Reg.leverage`.''')
@@ -143,7 +178,20 @@ class Reg(object):
 
 
     def influence(self):
-        # SOURCE: Hansen, Chapter 3
+        """Computes the largest (absolute) change in the predicted value due to
+        a single observation. If this diag- nostic is large relative to the
+        distribution of yi; it may indicate that that observation is ináuential.
+
+        Observation i is ináuential if its omission from the sample induces
+        a substantial change in a parameter estimate of interest.  Note that
+        a leverage point is not necessarily ináuential as the latter also
+        requires that the prediction error e is large.
+
+        SOURCES:
+
+            - Hansen, Chapter 3.21
+
+        """
         return (self.h * self.e_til).max()
 
 
