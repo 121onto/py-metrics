cf409480dae32009140e08097eb0ddc9677aad27
121onto <121onto@gmail.com>
2019-02-15 17:55:39 -0800

    Updates compute leverage

--- py_metrics/base.py
+++ py_metrics/base.py
@@ -6,6 +6,10 @@ from __future__ import division
 import numpy as np
 import pandas as pd
 
+from scipy.linalg import lstsq
+from numpy.linalg import inv
+from numpy.linalg import cholesky
+
 from py_metrics import caches
 
 ###########################################################################
@@ -14,10 +18,9 @@ from py_metrics import caches
 
 class Reg(object):
     def __init__(self, x, y):
-        from scipy.linalg import lstsq
-        self.lstsq = lstsq
         self.x = x
         self.y = y
+        self.h = None
 
         self._is_fit = False
         self.beta = None
@@ -28,12 +31,11 @@ class Reg(object):
         self.qxx = None
         self.qxy = None
 
-    def fit(self, frame):
-        from numpy.linalg import inv
 
+    def fit(self, frame):
         # Prepare the data
         x = frame[self.x].copy(deep=True).astype(np.float32).values
-        y = frame[self.y].copy(deep=True).astype(np.float32).values
+        y = frame[self.y].copy(deep=True).astype(np.float32).valuse
 
         # other operations
         self.qxx = np.matmul(np.transpose(x), x)
@@ -41,7 +43,7 @@ class Reg(object):
         self.qxx_inv = inv(self.qxx)
 
         # Fit the regression
-        self.beta, self.sse, self.rnk_beta, self.svs_beta = self.lstsq(x, y)
+        self.beta, self.sse, self.rnk_beta, self.svs_beta = lstsq(x, y)
         self._is_fit = True
 
         # Compute summary stats
@@ -49,9 +51,21 @@ class Reg(object):
         self.omega_hat = self.see / frame.shape[0]
         self.r2 = (1 - self.sse / self.ssy)
 
+
     def predict(frame):
         x = frame[self.x].copy(deep=True).astype(np.float32).values
         return np.matmul(x, self.beta)
+
+
+    def leverage(frame):
+        # SOURCE: https://stackoverflow.com/a/39534036/759442
+        if self.h is not None:
+            return self.h
+        x = frame[self.x].copy(deep=True).astype(np.float32).values
+        qxx_u = cholesky(self.qxx)
+        z = lstsq(np.transpose(qxx_u), np.transpose(x))
+        self.h = np.square(z).sum(axis=1)
+        return self.h
 
 
 ###########################################################################
